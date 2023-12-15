a5f40b9d816ec872a0c59da0a27808a05bc90e3a
Kenneth Vargas <kennethvargas26@gmail.com>
2023-12-15 22:38:52 +0800

    update home page layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-white font-sans transition-colors">
+
+        @include('layouts.navigation')
+
+        <!--Blog Post List -->
+        <div class="mx-auto flex flex-col items-center justify-center text-white w-11/12 md:w-full pb-20">
+            <x-posts.post-card></x-posts.post-card>
+            <x-posts.post-card></x-posts.post-card>
+        </div>
+        
+    </body>
+</html>
