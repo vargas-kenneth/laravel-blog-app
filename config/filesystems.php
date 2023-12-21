bd89a0a38dfb0da305b0efa902ffc96cb285295e
Kenneth Vargas <kennethvargas26@gmail.com>
2023-12-21 21:15:25 +0800

    add storage link

--- config/filesystems.php
+++ config/filesystems.php
@@ -71,6 +71,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
+        public_path('user_images') => storage_path('app/public/images/users'),
     ],
 
 ];
