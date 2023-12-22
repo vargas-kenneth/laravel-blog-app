0529a46aa41e65b217186aaae40ae1af5f5e02c0
Kenneth Vargas <kennethvargas26@gmail.com>
2023-12-22 22:00:57 +0800

    remove content associated with users and post from postImage factory

--- database/factories/PostImageFactory.php
+++ database/factories/PostImageFactory.php
@@ -20,23 +20,11 @@ class PostImageFactory extends Factory
      */
     public function definition(): array
     {
-        // it would be great to first find a post without a image and attach image to it 
-        // maybe create a seperate factory that find a post without image and attach image to it
-        $user = rand(1,3);
-        $post = Post::factory()->create();
-        
-        $postTitle = $post->title;
-        $userFullname = Str::slug(User::find($user)->name, '_');
-        $imagePath = 'user_images/'. $userFullname . '/';
         $catImages = ['cat-1.jpg', 'cat-2.jpg', 'cat-3.jpg'];
         $cat = fake()->randomElements($catImages);
         
         return [
-            'user_id' => $user,
-            'post_id' => $post,
-            'image_path' =>  $imagePath,
             'image_name' => $cat[0],
-            'image_alt' => $postTitle,
         ];
     }
 }
