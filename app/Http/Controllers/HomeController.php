3dce993b671d79420eca7261bd01c88b3d2c1113
Kenneth Vargas <kennethvargas26@gmail.com>
2024-01-12 22:23:58 +0800

    add implement data retrieval from Post

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use Illuminate\Support\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,12 +14,30 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
+        // $posts = Post::latest('updated_at')->get()->take(10);
+        // Eager loading
+        $posts = Post::with('user:id,name', 'postImage', 'tag')->latest('updated_at')->get();
+
+        $posts->each(function($post) {
+            $post->time_posted = $post->updated_at->diffForHumans();
+
+            if ($post->updated_at->diffInHours(Carbon::now()) > 24) {
+                $carbonDate = Carbon::parse($post->updated_at);
+                $post->time_posted = $carbonDate->format('F j, Y g:i A');
+            }
+
+            $post->img_full_path = $post->postImage->image_path . $post->postImage->image_name;
+        });
+
+        // Limit the post when using eager loading
+        $limitPost = $posts->take(10);
+        $data = ['posts' => $limitPost];
+        
         if (Auth::check()) {
             $user = Auth::user();
-            $firstName = explode(' ', $user->name)[0];
-            return view('home')->with('firstName', $firstName);
+            $data['firstName'] = explode(' ', $user->name)[0];
         }
 
-        return view('home');
+        return view('home', $data);
     }
 }
