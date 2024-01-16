5d450025300e9f788b5ccbd1fa3096ad8726f815
Kenneth Vargas <kennethvargas26@gmail.com>
2024-01-16 17:14:16 +0800

    update tinymce configuration

--- resources/views/components/head/tinymce-config.blade.php
+++ resources/views/components/head/tinymce-config.blade.php
@@ -1,15 +1,18 @@
-<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+<!-- Place the first <script> tag in your HTML's <head> -->
+<script src="https://cdn.tiny.cloud/1/w2984qw12z0pvzv4mebduuy7na8i7al22pnirbvmsropwke4/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 
+<!-- Place the following <script> and <textarea> tags your HTML's <body> -->
 <script>
-
-   tinymce.init({
-
-     selector: 'textarea#content', // Replace this CSS selector to match the placeholder element for TinyMCE
-
-     plugins: 'powerpaste advcode table lists checklist',
-
-     toolbar: 'undo redo | blocks| bold italic | bullist numlist checklist | code | table'
-
-   });
-
+  tinymce.init({
+    selector: 'textarea#content',
+    plugins: 'ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
+    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+    tinycomments_mode: 'embedded',
+    tinycomments_author: 'Author name',
+    mergetags_list: [
+      { value: 'First.Name', title: 'First Name' },
+      { value: 'Email', title: 'Email' },
+    ],
+    ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
+  });
 </script>
