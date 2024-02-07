2c7fb8ea50d1aa8910614b47a45eee4b62e931d8
Kenneth Vargas <kennethvargas26@gmail.com>
2024-02-07 22:36:31 +0800

    add post deleted event to event service provider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Events\PostCreatedEvent;
+use App\Events\PostDeletedEvent;
 use App\Listeners\SendPostCreated;
+use App\Listeners\SendPostDeleted;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -23,8 +25,11 @@ class EventServiceProvider extends ServiceProvider
         PostCreatedEvent::class => [
             SendPostCreated::class,
         ],
+        PostDeletedEvent::class => [
+            SendPostDeleted::class,
+        ],
     ];
-
+    
     /**
      * Register any events for your application.
      */
